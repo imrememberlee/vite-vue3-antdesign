dbbccff999bddb3fee69d6f2bfc6fcc459dea04a
Ben <imrememberlee@gmail.com>
2023-04-23 01:59:48 +0800

    init project and pinia

---
+++ src/components/usePinia.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{ userStore.name }}</div>
+  <button @click="edit">修改</button>
+</template>
+
+<script lang="ts" setup>
+import { useUserStore } from '@/store/user';
+
+const userStore = useUserStore();
+const edit = () => {
+  userStore.updateName('李四');
+};
+</script>
